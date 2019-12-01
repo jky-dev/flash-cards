02da09bd7647a11007e05feb4c27149d46398ed5
Jacky Liang <liangj97@gmail.com>
2019-12-01 15:51:33 +1100

    Reorganise imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { QuizComponent, PreferencesDialog } from './components/quiz/quiz.component';
+import { AllQuestionsComponent } from './components/all-questions/all-questions.component';
+import { environment } from 'src/environments/environment.prod';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment.prod';
-import { AllQuestionsComponent } from './components/all-questions/all-questions.component';
-import { MatButtonModule, MatCheckboxModule, MatDialogModule, MatIconModule, MatCardModule, MatExpansionModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
